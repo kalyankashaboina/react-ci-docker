7bea27f05701d7f36e60e0a02a090f87a62baa81
kalyankashaboina <kalyankashaboina07@gmail.com>
2025-12-06 12:26:01 +0530

    fix:checking husky

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import styles from './Button.module.scss';
-
-// Extends standard button props for full passthrough of attributes like aria-label, etc.
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  variant?: 'primary' | 'secondary'; // Example of a variant prop
+  variant?: 'primary' | 'secondary';
 }
 
 const Button: React.FC<ButtonProps> = ({ children, variant = 'primary', ...props }) => {
-  // Use the variant prop to apply different styles
   const buttonClass = variant === 'secondary' ? styles.buttonSecondary : styles.button;
-
   return (
     <button className={buttonClass} {...props}>
-      {children}
+      {' '}
+      {children}{' '}
     </button>
   );
 };
