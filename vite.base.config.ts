ba5fb496c8c24ba29fd7035e0dba39e9a46c2cb6
kalyankashaboina <kalyankashaboina07@gmail.com>
2025-12-06 16:16:19 +0530

    resolved styling issue

--- vite.base.config.ts
+++ vite.base.config.ts
@@ -5,9 +5,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-
   resolve: {
     alias: {
+      '@src': path.resolve(__dirname, 'src'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
@@ -15,13 +15,12 @@ export default defineConfig({
       '@store': path.resolve(__dirname, 'src/store'),
       '@services': path.resolve(__dirname, 'src/services'),
       '@constants': path.resolve(__dirname, 'src/constants'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@styles': path.resolve(__dirname, 'src/assets/styles'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@router': path.resolve(__dirname, 'src/router'),
       '@types': path.resolve(__dirname, 'src/types'),
     },
   },
-
   css: {
     preprocessorOptions: {
       scss: {
